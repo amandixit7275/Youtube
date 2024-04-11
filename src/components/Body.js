7fe91440eb09eace7230a10c85412d1390dbf5dc
Aman <aman.d6t@gmail.com>
2024-04-11 17:43:52 +0530

    initial commit

---
+++ src/components/Body.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Body() {
+  return <div>body Component</div>;
+}
+
+export default Body;
---
+++ src/components/Head.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Head = () => {
+  return (
+    <div>
+      <h2>Head Component</h2>
+    </div>
+  );
+};
+
+export default Head;
